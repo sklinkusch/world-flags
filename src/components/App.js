fb00565131ce382144b2b727f42bd74ece8c9c29
Stefan Klinkusch <stefan.klinkusch@googlemail.com>
2021-07-12 21:17:25 +0200

    rearrange files

---
+++ src/components/App.js
@@ -0,0 +1,35 @@
+/** @jsxImportSource theme-ui */
+import Flag from "react-world-flags";
+import flags from "../data/flags";
+
+function App() {
+  return (
+    <div className="App" sx={{ textAlign: "center" }}>
+      <header 
+        className="App-header" 
+        sx={{ 
+          backgroundColor: "#282c34", 
+          minHeight: "100vh", 
+          display: "flex", 
+          flexDirection: "column", 
+          justifyContent: "center", 
+          alignItems: "center", 
+          color: "white", 
+          fontSize: "calc(10px + 2vmin)"
+        }}
+      >
+        <div sx={{ 
+          display: "flex", 
+          flexDirection: "row", 
+          justifyContent: "space-around", 
+          alignItems: "center", 
+          flexWrap: "wrap" 
+        }}>
+          {flags.map(flag => <Flag code={flag.code} sx={{width: "100px", mx: "8px", my: "12px"}} />)}
+        </div>
+      </header>
+    </div>
+  );
+}
+
+export default App;
